9aedcb18dc54d988dc76804ad9baea76a7a096cc
Eric Lara <ericzon@gmail.com>
2017-03-21 07:52:02 +0100

    ADD new endpoint to find nearest points

--- src/controllers/wifipoint.controller.ts
+++ src/controllers/wifipoint.controller.ts
@@ -9,11 +9,19 @@ const Wifipoint = mongoose.model('wifipoint');
 export class WifipointController {
     public static getWifipoints(req: Request, res: Response) {
         Wifipoint.find().exec().then(function(wifiRes: any) {
-            logger.verbose("getWifipoints > GET ",wifiRes);
+            // logger.verbose("getWifipoints > GET ",wifiRes);
+            logger.verbose("getWifipoints > GET ");
             res.json({bundle: wifiRes});
         }, function(err: any) {
             logger.error("getWifipoints > ERR: ",err);
             res.status(500).end();
         });
+    }
+
+    public static getNearestWifipoints(req: Request, res: Response) {
+        let body = req.body;
+        // console.log(req);
+        logger.verbose("getNearestWifipoints > POST > body: ",body);
+        res.json(body);
     }
 }
--- src/routes/wifipoint-routes.ts
+++ src/routes/wifipoint-routes.ts
@@ -8,5 +8,6 @@ export default (app: any) => {
 	logger.info("Wifipoints route enabled");
 	const router = express.Router();
 	router.get('/wifipoints', WifipointController.getWifipoints);
+	router.post('/wifipoints/find-nearest', WifipointController.getNearestWifipoints);
 	app.use(router);
 };
